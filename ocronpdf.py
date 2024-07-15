1221986fbfcc44bf755e42d918035ed8ff7fa4bc
Ra-Na <gluege@boerde.de>
2024-07-15 14:48:27 +0200

    Update ocronpdf.py

--- ocronpdf.py
+++ ocronpdf.py
@@ -143,29 +143,10 @@ doc = fitz.open("output.pdf")
 for pageindex in range(len(texts)):
     text = texts[pageindex]
     page = doc[pageindex]
-    # get image resolution to calculate scaling on page
-    process = subprocess.run(['identify', pages[pageindex]], capture_output=True)
-    nums = re.findall(r'\d+', str(process.stdout))
-
-    imheight = float(nums[2])
-    imwidth = float(nums[1])
-    pgheight = float(pdf.eph+pdf.t_margin+pdf.b_margin)
-    pgwidth = float(pdf.epw+pdf.l_margin+pdf.r_margin)
-
-    pgar = pgheight/pgwidth
-    imar = imheight/imwidth
-    
-    if(pgar>imar):
-        scaling = pgwidth/imwidth
-    else:
-        scaling = pgheight/imheight
-
-    scaling = 1 
-
     for content in text:
-        textheight = float(content[0][2][1]-content[0][0][1])*scaling
-        textwidth  = float(content[0][1][0]-content[0][0][0])*scaling
-        lowerleft  = [float(content[0][0][0])*scaling,float(content[0][0][1])*scaling]
+        textheight = float(content[0][2][1]-content[0][0][1])
+        textwidth  = float(content[0][1][0]-content[0][0][0])
+        lowerleft  = [float(content[0][0][0]),float(content[0][0][1])]
         text_box = page.insert_textbox((lowerleft[0],lowerleft[1],lowerleft[0]+textwidth,lowerleft[1]+textheight), content[1], fontsize=12, fontname='helv', fontfile=None, color=None, fill=None, render_mode=0, border_width=1, expandtabs=8, rotate=0, morph=None, stroke_opacity=0, fill_opacity=0, oc=0, overlay=True)
     page.clean_contents(sanitize=True)
 
@@ -191,4 +172,3 @@ sizejb2 = os.path.getsize(sjb2)
 print(s   +": "+"          \t"+'{:>12,.0f}'.format(size))
 print(spng+": \t"+'{:>12,.0f}'.format(sizepng)+"\t"+str(round(100.0*float(sizepng-size)/float(size),2))+"%")
 print(sjb2+": \t"+'{:>12,.0f}'.format(sizejb2)+"\t"+str(round(100.0*float(sizejb2-size)/float(size),2))+"%")
-
